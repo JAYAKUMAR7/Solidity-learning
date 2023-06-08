023e787975386e0127f22070501a13fdb8423c08
JAYAKUMAR7 <89391565+JAYAKUMAR7@users.noreply.github.com>
2023-06-08 20:37:46 +0530

    initial commit

---
+++ StructReadData.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Struct{
+    struct Person{
+        string Name;
+        uint Age;
+    }
+
+    Person /* Variable */ public person;
+
+    function UpdateDetails(string memory _name , uint _age) public {
+        person = Person(_name , _age);
+        // (or else) person = Person({Name :_name , Age : _age});
+    }
+
+    function ReadData() public view returns(string memory) {
+        return person.Name;
+    }
+
+}
