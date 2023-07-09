28bb2bd78ac9b26718392598072909cd08594c59
JAYAKUMAR7 <89391565+JAYAKUMAR7@users.noreply.github.com>
2023-07-09 21:02:23 +0530

    initial commit

---
+++ Functions1.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Function{
+
+    // Function can return multiple values.
+
+    function returnMany() public pure returns(uint , bool, uint){
+        return (1, true, 2);
+    }
+    
+
+    //Returns values can be named.
+    function named() public pure returns(uint x, bool b, uint y){
+        return (1, true, 2);
+    }
+
+    //Returns value can be assigned to their name.
+    //In this case the return statement can be omitted.
+
+    function assigned() public pure returns(uint x, bool b, uint y){
+        x =1;
+        b = true;
+        y = 2;
+    }
+
+    //Use destructuring assignment when calling another function that returns multiple values.
+
+    function DestructuringAssignments() public pure returns(uint, bool, uint, uint, uint){
+        (uint i, bool b, uint j) = returnMany();
+
+        //values can be left out.
+        (uint x,uint y) = (4,5);
+        return(i,b,j,x,y);
+    }
+
+    //Cannot use map for either input or output
+
+    //can use array for input
+    function arrayInput(uint[] memory _arr) public {}
+
+    //Can use array for output
+    uint[] public arr;
+
+    function arrayOutput() public view returns(uint[] memory){
+        return arr;
+    }
+}
+
+//Call function with key-value inputs
+
+contract XYZ{
+    function SomeFunctionsWithManyInputs(uint x, uint y, uint z, address a, bool b, string memory c) public pure returns (uint){}
+
+    function callFunc() external pure returns (uint){
+
+        return SomeFunctionsWithManyInputs(1, 2, 3,address(0), true, "c");
+    }
+
+    function CallFuncWithKeyValue() external pure returns(uint){
+
+        return SomeFunctionsWithManyInputs({a: address(0), b: true, c: "c", x: 1, y: 2, z:3});
+    }
+}
